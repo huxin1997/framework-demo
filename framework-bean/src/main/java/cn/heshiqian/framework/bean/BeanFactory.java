004551f0d6364f12f3cbf8c260d8efbf72d069d6
1429 <1429@1429.com>
2018-09-25 18:53:02 +0800

    none

---
+++ framework-bean/src/main/java/cn/heshiqian/framework/bean/BeanFactory.java
@@ -0,0 +1,5 @@
+package cn.heshiqian.framework.bean;
+
+public class BeanFactory {
+    
+}
---
+++ framework-bean/src/test/java/BeanPackageTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+public class BeanPackageTest {
+
+    @Test
+    public void t1(){
+        System.out.println("a");
+    }
+
+}
